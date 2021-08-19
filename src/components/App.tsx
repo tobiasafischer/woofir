584c1a0fee146dc65aa62f422d001fdfb9e9620c
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-08-18 18:42:20 -0700

    initial styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Home from './Home/Home';
 
-const App = () => <div className="App">p</div>;
+const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+);
 
 export default App;
